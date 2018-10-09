4cb14bd86f2472129a11d46a0269a18b6dc5db81
Bryan Slatner <bsgit@slatner.com>
2018-10-08 20:47:01 -0400

    Don't redirect to https

--- src/AspNetCoreExample/Startup.cs
+++ src/AspNetCoreExample/Startup.cs
@@ -48,7 +48,7 @@ namespace AspNetCoreExample
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
